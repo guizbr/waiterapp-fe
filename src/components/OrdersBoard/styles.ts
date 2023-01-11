dee7e280a7fcfe5965f5e4d1ebe8e64dcd474df3
Guilherme Camargo <guilherme1290@outlook.com.br>
2023-01-11 14:37:31 -0300

    first commit

---
+++ src/components/OrdersBoard/styles.ts
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+
+export const Board = styled.div`
+  padding: 1rem;
+  border: 1px solid rgba(204, 204, 204, 0.4);
+  border-radius: 16px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+	flex: 1;
+
+  > header {
+    padding: 0.5rem;
+    font-size: 0.875rem;
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+  }
+`;
+
+export const OrderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 1.5rem;
+  width: 100%;
+
+  button {
+    background: #fff;
+    border: 1px solid rgba(204, 204, 204, 0.4);
+    height: 128px;
+    border-radius: 8px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 0.25rem;
+
+    strong {
+    font-weight: 500;
+    }
+
+    span {
+      font-size: 0.875rem;
+      color: #666;
+    }
+
+    & + button {
+      margin-top: 1.5rem;
+    }
+  }
+`;
